89000d162d461691825d1d56084dced03be1a96f
Jessica <jessicalim@bell.net>
2018-11-22 18:27:05 -0500

    adjusted menu with imports

--- dp2menu.py
+++ dp2menu.py
@@ -3,19 +3,9 @@ Author: Liam Ward and Jessica Lim
 Objective: DP2 Program Menu
 """
 import math
-#Variables:
-teamNumber = 14
-bodyWeight = 1
-modulusBone = 1
-modulusImplant = 1
-outerDia = 1 #Outer diameter of stem
-canalDiameter = 1#inner diameter of stem
-canalOffset = 1 #Canal offset from femoral head
-modulusBone = 1 #modulus of elasticity of cortical bone
-ultTenStrength = 1 # the ultimate strength of the implant
-modulusImplant = 1 # the elastic modulus of the implant
-stemDia = 1 #Diameter of the implant stem
-#Function to account for user input error:
+import diameterCalc 
+import stresslifeCalc 
+import var
 
 def main_menu():
     #Home menu setup:
@@ -44,11 +34,12 @@ def menu_func():
         #Accounts for user error in entering their subprogram choice
         sub_choice = error_func()
         if sub_choice == 1:
-            import dp2sub1
+            diameter = diameterCalc.minDiamCalc()
+            diameterCalc.statementPrinter(diameter)
         elif sub_choice == 2:
-            dia = "global"
-            dia = from dp2sub1 import sub1
-            import dp2sub2
+            diameter = diameterCalc.minDiamCalc()
+            amp, cycles = stresslifeCalc.returnCalculations(diameter)
+            print
         elif sub_choice == 3:
             import dp2sub3
         elif sub_choice == 4:
