3e6af1ef960d2a6e284d24275ff0759fdc335d44
Jessica <jessicalim@bell.net>
2018-11-21 23:28:29 -0500

    Added subprogram 2, to calculate fatigue life

---
+++ dp2sub2.py
@@ -0,0 +1,15 @@
+import os
+import math
+
+#teamNumber = 14
+bodyWeight = 3
+#outerDia = 6
+canalDiameter = 7
+canalOffset = 8
+#modulusBone = 9
+ultTenStrength = 45
+#modulusImplant = 3
+stemDia = 5
+width = os.get_terminal_size().columns
+
+
