0c086ce69cf8a906ce60fb2e128431e2bee420af
Jessica <jessicalim@bell.net>
2018-11-22 18:33:49 -0500

    changed the name of sub2 to returnCalculations

---
+++ stresslifeCalc.py
@@ -0,0 +1,62 @@
+import math
+import var
+
+#calculates the stress amplitude
+def stressAmp(dia):
+    area = math.pi * ((dia / 2) ** 2)
+    fail  = (10 * bodyWeight) / area
+    return fail
+
+#calculates the number of required cycles    
+def cycleCalc(stressFail):
+    currentStress = stressFail
+    cycles = -1
+    data = []
+    data = read_data("metaldata.txt") 
+    k = 6 + (math.log(10) ** (14/30))
+    #loops through the values of the file until the stressFail is bigger than the stress-strain curve        
+    for lines in data:
+        cycles = cycles + 1
+        #breaks if value is above curve
+        if lines[1] < currentStress: 
+            break
+        #continues if value is below the curve    
+        else:
+            currentStress = currentStress * k
+    return cycles
+
+#read the file            
+def read_data(filename):
+    in_file = open(filename,"r")
+    lines_list = []
+    #call the readlines function
+    lines_list = readlines(in_file)
+    in_file.close()
+    file_list = []
+    #iterate through the list of strings from the file
+    for line in lines_list:
+        split_str = line.split()
+        split_float = []
+        #convert each string into a float
+        for var in split_str:
+            split_float.append(float(var))
+        #add the float list to the file list of floats
+        file_list.append(split_float)
+    return file_list
+
+#read all the lines
+def readlines(filename):
+    all_lines = []
+    #read all the lines and add to list
+    for x in filename:
+        all_lines.append(x)
+    return all_lines
+    
+    #main function, calls other functions and prints the calculated values
+def returnCalculations(dia):
+    stressFail = stressAmp(dia)
+    cyclesFail = cycleCalc(stressFail)
+    #prints calculated values
+    #print("The implant will fail after %s cycles" % (cyclesFail))
+    #print("The maximum stress amplitude that corresponds to failture is ", stressFail)
+    return stressFail, cyclesFail
