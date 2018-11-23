a5094279e0a8c2d37bbc4fb8da3c373da87a134b
Jessica <jessicalim@bell.net>
2018-11-23 14:36:27 -0500

    Adjusted newton and check methods to change function

--- diameterCalc.py
+++ diameterCalc.py
@@ -28,33 +28,33 @@ def calculate(equation):
 #uses newton's nethod to determine the x intercept, which is equivallent to the radius of the bone
 def newtons(equation, min_diameter):
     # calculates the value of the function and the deriviative of the function, with the current diameter being evaluates
-    function = equation[0] * (min_diameter ** 3) - equation[1] * (min_diameter ** 2) - equation[2]
-    deriviative = equation[0] * 3  * (min_diameter ** 2) - equation[1] * 2 * min_diameter
+    function = equation[0] * (min_diameter ** 3) - equation[1] * (min_diameter)  - equation[2]
+    deriviative = equation[0] * 3  * (min_diameter ** 2) - equation[1]
     # evaluates x1 = x0 - f(x) / f'(x)
     min_diameter = min_diameter - (function / deriviative)
     return min_diameter
 
 #calculates the value of the value of the function with the newly calculated minimum diameter
 def check(equation, min_diameter):
-    zero = equation[0] * (min_diameter ** 3) - equation[1] * (min_diameter ** 2) - equation[2]
+    zero = equation[0] * (min_diameter ** 3) - equation[1] * (min_diameter) - equation[2]
     return zero
 
 #main program that calls other functions 
 def minDiam_calc():
     #calls other functions to make calculations
-    appTen_stress = var.body_weight * 3.5
-    a = axial(appTen_stress)
-    b  = bend(appTen_stress)
+    load = var.body_weight * 3.5
+    a = axial(load)
+    b  = bend(load)
     equation = [var.ultTen_strength, a, b]
-    minDia = calculate(equation)
-    return minDia, (a + b)
+    min_dia = calculate(equation)
+    app_tens = a / (min_dia ** 2) + b / (min_dia ** 3)
+    return min_dia, app_tens
 
 #calls sub1 and prints the required information
 def statement_printer(min_diameter, tens):
     #Prints the required information, involving the inputted data
     print("\tThe following calculations have been made for users with the following parameters")
-    print("\tBody Weight: ", round((var.body_weight / 9.8), 2), "kg")
-    print("\tCanal diameter: ", var.canal_diameter, "mm") 
-    print("\tZercanium with a tensile strength:  ", var.ultTen_strength, "MPa")
+    print(("\tBody Weight: %s kg\t\tCanal Diameter: %s mm\t\tZercanium tensile Strength %s MPa " % (round((var.body_weight / 9.8), 2), var.canal_diameter, var.ultTen_strength)))
     print(("\tThe minimum diameter that can be used for this implant is %s mm" % (round(min_diameter, 2))))
     print(("\tThe applied tensile strength for this implant is %s MPa" % (round(tens, 2))))
+
