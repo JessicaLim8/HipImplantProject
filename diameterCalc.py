1220943c327766daa397937cda5453f6a0d252b0
Jessica <jessicalim@bell.net>
2018-11-23 10:55:17 -0500

    changed to snake_case

--- diameterCalc.py
+++ diameterCalc.py
@@ -9,52 +9,52 @@ def axial(load):
 
 #calculates the bending compression strength
 def bend(load):
-    bending = (load * var.canalOffset * 32) / math.pi
+    bending = (load * var.canal_offset * 32) / math.pi
     return bending
 
 #Performs a loop that will iterate through values until a root is found (the root is the value of the diameter)
 def calculate(equation):
     #set initial values to allow for iteration
-    minDiameter = 5
+    min_diameter = 5
     zero = 10
     #loops until the value of the funcion using the chosen root is close enough to zero)
     while (zero > 0.001):
        # recalculates the value of diameter 
-       minDiameter =  newtons(equation, minDiameter)
+       min_diameter =  newtons(equation, min_diameter)
        #checks to see what the value of the function is with the new diameter
-       zero = check(equation, minDiameter)
-    return minDiameter
+       zero = check(equation, min_diameter)
+    return min_diameter
 
 #uses newton's nethod to determine the x intercept, which is equivallent to the radius of the bone
-def newtons(equation, minDiameter):
+def newtons(equation, min_diameter):
     # calculates the value of the function and the deriviative of the function, with the current diameter being evaluates
-    function = equation[0] * (minDiameter ** 3) - equation[1] * (minDiameter ** 2) - equation[2]
-    deriviative = equation[0] * 3  * (minDiameter ** 2) - equation[1] * 2 * minDiameter
+    function = equation[0] * (min_diameter ** 3) - equation[1] * (min_diameter ** 2) - equation[2]
+    deriviative = equation[0] * 3  * (min_diameter ** 2) - equation[1] * 2 * min_diameter
     # evaluates x1 = x0 - f(x) / f'(x)
-    minDiameter = minDiameter - (function / deriviative)
-    return minDiameter
+    min_diameter = min_diameter - (function / deriviative)
+    return min_diameter
 
 #calculates the value of the value of the function with the newly calculated minimum diameter
-def check(equation, minDiameter):
-    zero = equation[0] * (minDiameter ** 3) - equation[1] * (minDiameter ** 2) - equation[2]
+def check(equation, min_diameter):
+    zero = equation[0] * (min_diameter ** 3) - equation[1] * (min_diameter ** 2) - equation[2]
     return zero
 
 #main program that calls other functions 
-def minDiamCalc():
+def minDiam_calc():
     #calls other functions to make calculations
-    appTenStress = var.bodyWeight * 3.5
-    a = axial(appTenStress)
-    b  = bend(appTenStress)
-    equation = [var.ultTenStrength, a, b]
+    appTen_stress = var.body_weight * 3.5
+    a = axial(appTen_stress)
+    b  = bend(appTen_stress)
+    equation = [var.ultTen_stength, a, b]
     minDia = calculate(equation)
     return minDia, (a + b)
 
 #calls sub1 and prints the required information
-def statementPrinter(minDiameter, tens):
+def statement_printer(min_diameter, tens):
     #Prints the required information, involving the inputted data
     print("\tThe following calculations have been made for users with the following parameters")
-    print("\tBody Weight: ", (var.bodyWeight / 9.8), "kg")
-    print("\tCanal diameter: ", var.canalDiameter, "mm") 
-    print("\tZercanium with a tensile strength:  ", var.ultTenStrength, "MPa")
-    print(("\tThe minimum diameter that can be used for this implant is %s mm" % (minDiameter)))
+    print("\tBody Weight: ", (var.body_weight / 9.8), "kg")
+    print("\tCanal diameter: ", var.canal_diameter, "mm") 
+    print("\tZercanium with a tensile strength:  ", var.ultTen_stength, "MPa")
+    print(("\tThe minimum diameter that can be used for this implant is %s mm" % (min_diameter)))
     print(("\tThe applied tensile strength for this implant is %s MPa" % (tens)))
