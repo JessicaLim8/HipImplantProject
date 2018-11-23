898f343565d822bfe89d4c44831541bf9764fbc7
Jessica <jessicalim@bell.net>
2018-11-23 10:45:58 -0500

    changed to snake case

--- stresslifeCalc.py
+++ stresslifeCalc.py
@@ -2,27 +2,26 @@ import math
 import var
 
 #calculates the stress amplitude
-def stressAmp(dia):
+def stress_amp(dia):
     area = math.pi * ((dia / 2) ** 2)
-    fail  = (10 * var.bodyWeight) / area
+    fail  = (10 * var.body_weight) / area
     return fail
 
 #calculates the number of required cycles    
-def cycleCalc(stressFail):
-    currentStress = stressFail
+def cycle_calc(stress_fail):
+    current_stress = stress_fail
     cycles = -1
     data = []
     data = read_data("metaldata.txt") 
-    k = 6 + (math.log(10) ** (14/30))
-    #loops through the values of the file until the stressFail is bigger than the stress-strain curve        
+    #loops through the values of the file until the stress_fail is bigger than the stress-strain curve        
     for lines in data:
         cycles = cycles + 1
         #breaks if value is above curve
-        if lines[1] < currentStress: 
+        if lines[1] < current_stress: 
             break
         #continues if value is below the curve    
         else:
-            currentStress = currentStress * k
+            current_stress = current_stress * (6 + math.log10(cycles) ** (14 / 30)) 
     return cycles
 
 #read the file            
@@ -30,7 +29,7 @@ def read_data(filename):
     in_file = open(filename,"r")
     lines_list = []
     #call the readlines function
-    lines_list = readlines(in_file)
+    lines_list = read_lines(in_file)
     in_file.close()
     file_list = []
     #iterate through the list of strings from the file
@@ -45,7 +44,7 @@ def read_data(filename):
     return file_list
 
 #read all the lines
-def readlines(filename):
+def read_lines(filename):
     all_lines = []
     #read all the lines and add to list
     for x in filename:
@@ -53,10 +52,10 @@ def readlines(filename):
     return all_lines
     
     #main function, calls other functions and prints the calculated values
-def returnCalculations(dia):
-    stressFail = stressAmp(dia)
-    cyclesFail = cycleCalc(stressFail)
+def return_calculations(dia):
+    stress_fail = stress_amp(dia)
+    cycles_fail = cycle_calc(stress_fail)
     #prints calculated values
-    #print("The implant will fail after %s cycles" % (cyclesFail))
-    #print("The maximum stress amplitude that corresponds to failture is ", stressFail)
-    return stressFail, cyclesFail
+    #print("The implant will fail after %s cycles" % (cycles_fail))
+    #print("The maximum stress amplitude that corresponds to failture is ", stress_fail)
+    return stress_fail, cycles_fail
