813ebacea78ae3c82a4c77497e1f2269a68392bf
Jessica <jessicalim@bell.net>
2018-11-23 11:26:42 -0500

    changed to snake case

--- yearStress.py
+++ yearStress.py
@@ -1,27 +1,27 @@
 import math
 import var
 
-areaBone = (math.pi * (var.canalDiameter/2)**2)
-ERatio = math.sqrt((var.modulusImplant/var.modulusBone))
+area_bone = (math.pi * (var.canal_diameter/2)**2)
+e_ratio = math.sqrt((var.modulus_implant/var.modulus_bone))
 
-def failureCalc():
+def failure_calc():
     #This program calculate the number of years before the femur will fracture
     years = 1
-    compStress = ((30 * var.bodyWeight)/ areaBone) #Stress from an axial load
-    compStrength = program3_compStrength(years) #Defines strength of the bone based on years since implant
-    stressReduc = compStress * ((2 * var.modulusBone)/(var.modulusBone + var.modulusImplant)) **(1/2)
+    comp_stress = ((30 * var.body_weight)/ area_bone) #Stress from an axial load
+    comp_strength = program3_comp_strength(years) #Defines strength of the bone based on years since implant
+    stress_reduc = comp_stress * ((2 * var.modulus_bone)/(var.modulus_bone + var.modulus_implant)) **(1/2)
     #condition for possible femur fracture
-    while stressReduc <= compStrength:
-        compStress = ((30 * var.bodyWeight)/ areaBone)
-        compStrength = program3_compStrength(years)
-        stressReduc = compStress * ((2 * var.modulusBone)/(var.modulusBone + var.modulusImplant)) **(1/2)
+    while stress_reduc <= comp_strength:
+        comp_stress = ((30 * var.body_weight)/ area_bone)
+        comp_strength = program3_comp_strength(years)
+        stress_reduc = comp_stress * ((2 * var.modulus_bone)/(var.modulus_bone + var.modulus_implant)) **(1/2)
         years += 1
     years -= 1
-    yrsFail = years
-    print("\tYour implant will fail in approximately", yrsFail, "years.") #Returns number of years until failure
-    stressFail = program3_compStrength(yrsFail) #Calculates stress associated with fracture
-    print("\tThe compressive stress will be equal to:", (-1 *(round(stressFail, 2))), " MPa when the femur fractures.")
+    yrs_fail = years
+    print("\tYour implant will fail in approximately", yrs_fail, "years.") #Returns number of years until failure
+    stressFail = program3_comp_strength(yrs_fail) #Calculates stress associated with fracture
+    print("\tThe compressive stress will be equal to:", round(stressFail, 2)," MPa when the femur fractures.")
 
-def program3_compStrength(years):
-    compStrength = ((0.001 * (years ** 2)) - (3.437 * years * ERatio) + 181.72)
-    return compStrength
+def program3_comp_strength(years):
+    comp_strength = ((0.001 * (years ** 2)) - (3.437 * years * e_ratio) + 181.72)
+    return comp_strength
