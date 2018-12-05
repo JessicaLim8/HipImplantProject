c47edb1bea045abc5ad3f7477de62105218928a8
Jessica <jessicalim@bell.net>
2018-12-04 22:00:41 -0500

    added comments

--- dp2menu.py
+++ dp2menu.py
@@ -1,12 +1,12 @@
 """
-Author: Liam Ward and Jessica Lim
+Authors: Liam Ward and Jessica Lim
 Objective: DP2 Program Menu
 """
 import math
-import diameterCalc 
-import stresslifeCalc 
-import yearStress
-import var
+import diameterCalc #importing subprogram 1
+import stresslifeCalc #importing subprogram 2
+import yearStress #importing subprogram 3
+import var #variables are defined in a seperate file
 
 def main_menu():
     #Home menu setup:
@@ -16,7 +16,7 @@ def main_menu():
     print("\t3. Subprogram 3 - Lifetime before Failure")
     print("\t4. Exit the Program")
 
-#error checking function
+#function to account for user input error:
 def error_func():
     x = 0
     while x == 0:
@@ -26,7 +26,7 @@ def error_func():
             print("") #add empty line for formatting
             break
         except ValueError:
-            print("You have entered an invalid input, please try again.\n\t If you would like to see the menu again, press 0 ")
+            print("\nYou have entered an invalid input, please try again.\n\t If you would like to see the menu again, press 0 ")
     return sub_choice
 
 #Menu Selection Function:
@@ -36,27 +36,27 @@ def menu_func():
         #Accounts for user error in entering their subprogram choice
         sub_choice = error_func()
         if sub_choice not in range(0, 5): 
-            print(f"Sorry, this program does not exist.\n\t If you would like to see the menu again, press 0")
+            print(f"\t Sorry, this program does not exist.\n\t If you would like to see the menu again, press 0")
             continue
-        if sub_choice == 1:
+        if sub_choice == 1: #runs the first subprogram
             diameter, app_tens = diameterCalc.minDiam_calc()
             diameterCalc.statement_printer(diameter, app_tens)
-        elif sub_choice == 2:
+        elif sub_choice == 2: #runs the second subprogram
             diameter, app_tens = diameterCalc.minDiam_calc()
             amp, cycles, fails = stresslifeCalc.return_calculations(diameter)
-            if not fails:
+            if not fails: #if the failure condition is never satisfied
                 print("\t The implant never fails")
             else:
-                print("\t The implant will fail after %s cycles" % (round(cycles, 2)))
-                print("\t The maximum stress amplitude that corresponds to failture is ", round(amp, 2))
-        elif sub_choice == 3:
+                print(f"\tThe implant will fail after %s cycles" % (round(cycles, 2)))
+                print(f"\tThe maximum stress amplitude that corresponds to failture is %s MPa " % (round(amp, 2)))
+        elif sub_choice == 3: #runs subprogram 3
             yearStress.failure_calc()
-        elif sub_choice == 4:
+        elif sub_choice == 4: #exits the program
             print("Thank you for using our stress analysis program, goodbye.")
             break
-        elif sub_choice == 0:
+        elif sub_choice == 0: #displays the menu again
             main_menu()
-        print("\nWe hope this information was helpful!\n\t If you would like to see the menu again, press 0")
+        print("\n\tWe hope this information was helpful!\nIf you would like to see the menu again, press 0")
 
 main_menu()
 menu_func()
--- yearStress.py
+++ yearStress.py
@@ -1,27 +1,25 @@
 import math
 import var
 
-area_bone = (math.pi/4) * ((var.outer_dia ** 2) - (var.canal_diameter ** 2))
-e_ratio = math.sqrt((var.modulus_implant/var.modulus_bone))
+area_bone = (math.pi/4) * ((var.outer_dia ** 2) - (var.canal_diameter ** 2)) #Defines the cross sectional area of the femur
+e_ratio = math.sqrt((var.modulus_implant/var.modulus_bone)) #Ratio of the modulus of the implant to the bone
 
 def failure_calc():
     #This program calculate the number of years before the femur will fracture
     years = 1
     comp_stress = ((30 * var.body_weight)/ area_bone) #Stress from an axial load
     comp_strength = program3_comp_strength(years) #Defines strength of the bone based on years since implant
-    stress_reduc = comp_stress * ((2 * var.modulus_bone)/(var.modulus_bone + var.modulus_implant)) **(1/2)
-    #condition for possible femur fracture
+    stress_reduc = comp_stress * ((2 * var.modulus_bone)/(var.modulus_bone + var.modulus_implant)) **(1/2) #reduced stress de to stress sheilding 
+    #condition for possible femur fracture:
     while stress_reduc <= comp_strength:
-        comp_stress = ((30 * var.body_weight)/ area_bone)
         comp_strength = program3_comp_strength(years)
-        stress_reduc = comp_stress * ((2 * var.modulus_bone)/(var.modulus_bone + var.modulus_implant)) **(1/2)
         years += 1
-    years -= 1
+    years -= 1 
     yrs_fail = years
     print("\tYour implant will fail in approximately", yrs_fail, "years.") #Returns number of years until failure
     stressFail = program3_comp_strength(yrs_fail) #Calculates stress associated with fracture
     print("\tThe compressive stress will be equal to:", round(stressFail, 2)," MPa when the femur fractures.")
 
 def program3_comp_strength(years):
-    comp_strength = ((0.001 * (years ** 2)) - (3.437 * years * e_ratio) + 181.72)
+    comp_strength = ((0.001 * (years ** 2)) - (3.437 * years * e_ratio) + 181.72) #equation for compressive strength of bone related to change in time
     return comp_strength
